fbdc35e2d279a44927434bbd0794408c6a8713d2
Gabriel Lopes <gblsilvalopes@gmail.com>
2022-04-21 19:44:12 -0300

    Implementa animação

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:food_deliveryapp/utils/colors.dart';
+import 'package:food_deliveryapp/widgets/big_text.dart';
+import 'package:food_deliveryapp/widgets/small_text.dart';
+import 'package:get/utils.dart';
+
+import '../widgets/IconAndTextWidget.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -8,11 +14,32 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  double _currentPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         height: 320,
         child: PageView.builder(
+            controller: pageController,
             itemCount: 5,
             itemBuilder: (context, index) {
               return _buildPageItem(index);
@@ -20,16 +47,102 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.symmetric(horizontal: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage('assets/image/food1.png'),
-        ),
+    Matrix4 matrix = new Matrix4.identity();
+
+    if (index == _currentPageValue.floor()) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, _height * (1 - _scaleFactor) / 2, 1);
+    }
+
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/image/food0.png'),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+                height: 120,
+                margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white),
+                child: Container(
+                  padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      BigText(
+                        text: 'Chinese Side',
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Wrap(
+                              children: List.generate(
+                                  5,
+                                  (index) => Icon(Icons.star,
+                                      color: AppColors.mainColor, size: 15))),
+                          SizedBox(width: 10),
+                          SmallText(text: '4.5'),
+                          SizedBox(width: 10),
+                          SmallText(text: '1287 comments'),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconAndTextWidget(
+                            text: 'Normal',
+                            icon: Icons.circle_sharp,
+                            iconColor: AppColors.iconColor1,
+                          ),
+                          IconAndTextWidget(
+                            text: '1.7km',
+                            icon: Icons.location_on,
+                            iconColor: AppColors.mainColor,
+                          ),
+                          IconAndTextWidget(
+                            text: '32min',
+                            icon: Icons.access_time_rounded,
+                            iconColor: AppColors.iconColor2,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )),
+          ),
+        ],
       ),
     );
   }
